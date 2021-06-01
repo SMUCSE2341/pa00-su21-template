50eb2e26fa45216cc232ecf3b77081e4eb2de298
Mark Fontenot <mfonten@lyle.smu.edu>
2021-06-01 10:55:58 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project 00 - Workflow
+
+_CS 2341 - Summer 21_
+
+## To Do: 
+Add code to `main.cpp` to introduce yourself to us. 
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    //Add a brief personal bio/introduction about yourself
+
+
+    return 0;
+}
